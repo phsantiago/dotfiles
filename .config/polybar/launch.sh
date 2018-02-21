08fa73f392def5c9807dabe1283e46180f1bbdec
Pedro Henrique Santiago <pedroh_s_@hotmail.com>
2018-02-20 23:53:09 -0300

    [bars] poly config, spotify integration

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+killall -q polybar
+
+polybar_proc=$(pgrep -u $UID -x polybar)
+
+# Terminate already running bar instances
+for i in ${polybar_proc} ; do
+    kill -9 $i
+done
+
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch bar top & bottom
+polybar top &
+polybar bottom &
+
+echo "Bars launched..."
